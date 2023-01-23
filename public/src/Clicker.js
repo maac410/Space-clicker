0120be96bee33fdd0aaff030b880d63dfc85ed0f
maac410 <maac410@gmail.com>
2023-01-23 12:56:02 -0500

    polish

--- public/src/Clicker.js
+++ public/src/Clicker.js
@@ -4,50 +4,42 @@ let upgradeCost = 10;
 let buttonOnOff = false;
 
 let Cookie = document. getElementById("cookie");
-let js = document. getElementById("Upgrade");
+let Upgrade = document. getElementById("Upgrade");
 let js2 = document.getElementById("upgrade");
 let js3 = document.getElementById("upgrade cost");
 
 function button(){
-
-if (clciks >= upgradeCost){
-  buttonOnOff = true;
-
- } else {
-
-  buttonOnOff = false;
-}
-
-switch(buttonOnOff){
-  case false:
-    js.disabled = true;
-    js.value = "UPGRADE NOT AVALIBLE";
-    
-    break;
-
-  case true:
-    js.disabled = false;
-    js.value = "UPGRADE AVALIBLE!";
-    break;
-}}
-
-function cClick() {
-  
   clciks = clciks + currentUpgrade;
   Cookie.value = clciks;
 
+if (clciks <= upgradeCost  ){
+  Upgrade.disabled = true;
+  buttonOnOff = true;
+  Upgrade.value = "...";
 }
+  else{
+    Upgrade.disabled = false;
+    buttonOnOff = false;
+    Upgrade.value = "...!";
+}}
 
 function upGrade() {
-
   currentUpgrade = currentUpgrade + 1;
   clciks = clciks - upgradeCost;
   upgradeCost = upgradeCost + 10
+}
 
+function upGradeDis(){
   Cookie.value = clciks;
   js2.innerHTML = currentUpgrade;
   js3.innerHTML = upgradeCost;
-  js.addEventListener("click", button);
-
+  console.log("It works");
 }
-console.log("it works")
+
+Cookie.addEventListener("click", button);
+Upgrade.addEventListener('click',() => {    
+  upGrade();
+  upGradeDis();  
+  button();  
+});
+upGradeDis();

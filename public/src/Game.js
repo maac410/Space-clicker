fe9f1a3d93d9305bdf821c669290be8acfe0becc
maac410 <maac410@gmail.com>
2023-01-24 13:13:03 -0500

    Canvas

--- public/src/Game.js
+++ public/src/Game.js
@@ -2,6 +2,9 @@ const Ship = document.getElementsByClassName('Ship');
 const ShipSprite = document.querySelector(".Ship");
 const pos = {x:0, y:0};
 
+let c = document.getElementById("Space");
+let ctx = c.getContext("2d");
+
 document.addEventListener("keydown", (ev) => {
 
     const dir = (ev.key.match(/(?<=^Arrow)\w+/)||[])[0];
@@ -16,3 +19,11 @@ document.addEventListener("keydown", (ev) => {
     
     ShipSprite.style.transform = `translate(${pos.x}px`
   });
+
+
+c.width= 300;
+c.height= 700;
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 1000, 1000);
+ctx.fillStyle = "purple";
+ctx.fillRect(130, 560, 40, 40);

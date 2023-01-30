9010486e023a15864f6181449677e25f1ce94715
maac410 <maac410@gmail.com>
2023-01-30 13:31:07 -0500

    Changes

--- public/src/Game.js
+++ public/src/Game.js
@@ -1,29 +1,53 @@
 const Ship = document.getElementsByClassName('Ship');
 const ShipSprite = document.querySelector(".Ship");
-const pos = {x:0, y:0};
-
 let c = document.getElementById("Space");
 let ctx = c.getContext("2d");
 
-document.addEventListener("keydown", (ev) => {
+let Cookie = document. getElementById("cookie");
 
-    const dir = (ev.key.match(/(?<=^Arrow)\w+/)||[])[0];
-    if (!dir) return; // Not an arrow key.
-    
-    ev.preventDefault(); // Prevent Browser scroll if overflow
+let posX = 145;
+let posY = 520;
+let width = 10;
+let height = 10;
+
+
+function drawShip (){
+  ctx.fillStyle = "purple";
+  ctx.fillRect(130, 560, 40, 30);
+  ctx.fillRect(140, 550, 20, 20);
+}
+
+function drawShot (){
+
+  ctx.fillStyle = "white";
+  ctx.fillRect(posX,posY,width,height);
+  ctx.fillStyle = "white";
+  ctx.fillRect(posX, posY - 300,width,height);
   
-    ({
-      Left:  () => pos.x -= 10,
-      Right: () => pos.x += 10,
-    }[dir])(); 
-    
-    ShipSprite.style.transform = `translate(${pos.x}px`
-  });
+  console.log(posX);
 
+}
+
+function drawEnemy (){
+  ctx.fillStyle = "green";
+  ctx.fillRect(125, 200, 50, 40);
+}
+
+//function hit(){
+  //if(){
+
+  //}
+
+//}
 
 c.width= 300;
 c.height= 700;
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, 1000, 1000);
-ctx.fillStyle = "purple";
-ctx.fillRect(130, 560, 40, 40);
+
+drawEnemy();
+drawShip();
+
+Cookie.addEventListener("click", drawShot)
+
+

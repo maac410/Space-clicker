cffc09df0603f4358e2795f9c67abbf079c7fa64
maac410 <maac410@gmail.com>
2023-02-02 19:35:30 -0500

    Shot works kinda

--- public/src/Game.js
+++ public/src/Game.js
@@ -1,15 +1,28 @@
-const Ship = document.getElementsByClassName('Ship');
-const ShipSprite = document.querySelector(".Ship");
 let c = document.getElementById("Space");
 let ctx = c.getContext("2d");
-
+let cShot = c.getContext("2d");
 let Cookie = document. getElementById("cookie");
 
-let posX = 145;
-let posY = 520;
-let width = 10;
-let height = 10;
+c.width = 300;
+c.height = 700;
+c.style.background = 'black';
 
+
+class Shot { constructor(posX, posY, width, height,shotColor){
+  this.posX = posX;
+  this.posY  = posY;
+  this.width  = width;
+  this.height  = height;
+  this.shotColor = shotColor;
+}
+drawShot (cShot){
+  cShot.fillStyle = this.shotColor;
+  cShot.fillRect(this.posX,this.posY,this.width,this.height);
+  console.log("shot fired");
+}}
+
+let shot = new Shot(148, 500, 5, 30, 'white');
+Cookie.addEventListener("click",() => shot.drawShot(cShot)) ;
 
 function drawShip (){
   ctx.fillStyle = "purple";
@@ -17,37 +30,10 @@ function drawShip (){
   ctx.fillRect(140, 550, 20, 20);
 }
 
-function drawShot (){
-
-  ctx.fillStyle = "white";
-  ctx.fillRect(posX,posY,width,height);
-  ctx.fillStyle = "white";
-  ctx.fillRect(posX, posY - 300,width,height);
-  
-  console.log(posX);
-
-}
-
 function drawEnemy (){
   ctx.fillStyle = "green";
   ctx.fillRect(125, 200, 50, 40);
 }
 
-//function hit(){
-  //if(){
-
-  //}
-
-//}
-
-c.width= 300;
-c.height= 700;
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, 1000, 1000);
-
 drawEnemy();
 drawShip();
-
-Cookie.addEventListener("click", drawShot)
-
-

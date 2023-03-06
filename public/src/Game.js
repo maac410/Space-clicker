f11900779062e00ae2d4a14634dcc4a777433596
maac410 <maac410@gmail.com>
2023-03-06 05:34:59 -0500

    Changes sprites

--- public/src/Game.js
+++ public/src/Game.js
@@ -4,28 +4,7 @@ let cShot = c.getContext("2d");
 let cShot2 = c.getContext("2d");
 let Alien = c.getContext("2d");
 let Cookie = document. getElementById("cookie");
-
-class Enemy { constructor(EposX, EposY, Ewidth, Eheight,EColor){
-  this.EposX = EposX;
-  this.EposY  = EposY;
-  this.Ewidth  = Ewidth;
-  this.Eheight  = Eheight;
-  this.EColor = EColor;
-  
-}
-  async drawEnemy (Alien){
-
-  let url = "../../Assets/Sprites/Alien.png";
-  let img = new Image();
-  await new Promise(r => img.onload=r, img.src=url);
-
-  Alien.fillStyle = this.EColor;
-  Alien.drawImage(img,this.EposX,this.EposY,this.Ewidth,this.Eheight);
-  console.log("Alien Attacking");
-}}
-
-let alien = new Enemy(120, 100, 50, 40, 'white');
-alien.drawEnemy(Alien);
+let alienIsDead = false;
 
 class Shot { constructor(posX, posY, width, height,shotColor, speed){
   this.posX = posX;
@@ -34,15 +13,13 @@ class Shot { constructor(posX, posY, width, height,shotColor, speed){
   this.height  = height;
   this.shotColor = shotColor;
   this.speed = speed;
-
   this.dy = 1 * this.speed;
 }
 drawShot (cShot){
   cShot.fillStyle = this.shotColor;
   cShot.fillRect(this.posX,this.posY,this.width,this.height);
-  console.log("shot fired");
+  console.log(this.posY);
 }
-
 drawShot2(cShot2){
   cShot2.fillStyle="black";
   cShot2.clearRect(this.posX-50,this.posY+20,this.width*50,this.height);
@@ -51,26 +28,57 @@ update(){
   this.drawShot(cShot);
   this.drawShot2(cShot2);
   this.posY += this.dy;
-}
-}
+}}
+let shot = new Shot(148, 380, 5, 30, 'white', -20);
 
-let shot = new Shot(148, 500, 5, 30, 'white', -30);
-
+class Enemy { constructor(EposX, EposY, Ewidth, Eheight,EColor){
+  this.EposX = EposX;
+  this.EposY  = EposY;
+  this.Ewidth  = Ewidth;
+  this.Eheight  = Eheight;
+  this.EColor = EColor;
+}
+  async drawEnemy (Alien){
+  let url = "../../Assets/Sprites/Alien 01.png";
+  let img = new Image();
+  await new Promise(r => img.onload=r, img.src=url);
+  Alien.fillStyle = this.EColor;
+  Alien.drawImage(img,this.EposX,this.EposY,this.Ewidth,this.Eheight);
+  console.log("Alien Attacking");
+}
+  async drawExplosion (){
+  let url = "../../Assets/Sprites/explosion.gif";
+  let img = new Image();
+  await new Promise(r => img.onload=r, img.src=url);
+  alienIsDead=true;
+  Alien.fillStyle = this.EColor;
+  Alien.drawImage(img,10,-200,300,600);
+  console.log("Alien is fucking dead");
+}}
+let alien = new Enemy(120, 100, 50, 40, 'white');
+alien.drawEnemy(Alien);
 
 let updateShot = function(){
-
   requestAnimationFrame(updateShot);
-  shot.update();
+
+  if(shot.posY >=  alien.EposY){
+    shot.update();
+    alien.drawEnemy(Alien);
+  } else {
+    Alien.clearRect(alien.EposX,alien.EposY,alien.Ewidth,alien.Eheight);
+    alien.drawExplosion();
+    
+  }}
+
+Cookie.addEventListener("click",() => {shot.drawShot(cShot), updateShot(), shot.posY= 550;});
+
+async function drawShip (){
+  let url = "../../Assets/Sprites/Ship 01.png";
+  let img = new Image();
+  await new Promise(r => img.onload=r, img.src=url);
+  Alien.drawImage(img,110,600,70,40);
+  console.log("ship");
 }
-
-Cookie.addEventListener("click",() => {shot.drawShot(cShot), updateShot()});
-
-function drawShip (){
-  ctx.fillStyle = "purple";
-  ctx.fillRect(130, 560, 40, 30);
-  ctx.fillRect(140, 550, 20, 20);
-}
-
 
 c.width = 300;
 c.height = 700;
